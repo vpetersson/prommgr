a7f814cf9683c717ec99aab59ddfcdaaa33b92fe
Viktor Petersson <vpetersson@wireload.net>
2017-01-12 17:52:56 +0000

    Fixes search bug in Django admin.

--- app/restapi/admin.py
+++ app/restapi/admin.py
@@ -5,16 +5,16 @@ from .models import Server
 class ServerAdmin(admin.ModelAdmin):
     list_display = (
         'server_ip',
-        'cloudnet_server_id',
         'cloudnet_owner_id',
+        'cloudnet_server_id',
         'created_at',
         'updated_at',
         'deleted',
     )
     search_fields = (
+        'cloudnet_owner_id',
         'cloudnet_server_id',
         'server_ip',
-        'owner',
     )
     list_filter = (
         'created_at',
