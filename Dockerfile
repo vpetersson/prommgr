ac3bc10be3c6675de07fe85c7460b3aa7952d17d
Viktor Petersson <vpetersson@wireload.net>
2017-03-05 16:09:57 +0000

    More static tweaks.

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.5
 COPY app /app
 RUN pip install -r /app/requirements.txt
 WORKDIR /app
+RUN python manage.py collectstatic --noinput
 ENV PYTHONUNBUFFERED 1
 CMD [ "gunicorn", "-b", "0.0.0.0:8000", "--workers", "4", "prommgr.wsgi" ]
--- app/prommgr/settings.py
+++ app/prommgr/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'prommgr.urls'
@@ -147,14 +148,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
